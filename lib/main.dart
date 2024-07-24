e3cadf84887f0d07081ed006227dbc0d9f8b9ab3
mabdula2004 <muhammadabdula7874747@gmail.com>
2024-07-24 07:00:46 -0700

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quiz_brain.dart';
+import 'package:neon_circular_timer/neon_circular_timer.dart';
 
 QuizBrain quizBrain = QuizBrain();
 
@@ -43,11 +44,14 @@ class _QuizPageState extends State<QuizPage> {
   ];
   int currentColorIndex = 0;
 
-  void checkAnswer(bool userPickedAnswer) {
+  final CountDownController _controller = CountDownController();
+
+  void checkAnswer(bool? userPickedAnswer) {
     bool? correctAnswer = quizBrain.getCorrectAnswer();
 
     setState(() {
       if (quizBrain.isFinished()) {
+        _controller.pause();
         showAlert(context, 'Finished!', 'You\'ve reached the end of the quiz.');
 
         quizBrain.reset();
@@ -56,15 +60,18 @@ class _QuizPageState extends State<QuizPage> {
         falseCount = 0;
         currentColorIndex = 0;
       } else {
-        if (userPickedAnswer == correctAnswer) {
-          scoreKeeper.add(
-            Icon(
-              Icons.check,
-              color: Colors.green,
-            ),
-          );
-          trueCount++;
-        } else {
+        if (userPickedAnswer == null) {
+          // Only add one cross if no answer is picked
+          if (scoreKeeper.isEmpty || scoreKeeper.last.icon != Icons.close) {
+            scoreKeeper.add(
+              Icon(
+                Icons.close,
+                color: Colors.red,
+              ),
+            );
+            falseCount++;
+          }
+        } else if (userPickedAnswer != correctAnswer) {
           scoreKeeper.add(
             Icon(
               Icons.close,
@@ -72,9 +79,18 @@ class _QuizPageState extends State<QuizPage> {
             ),
           );
           falseCount++;
+        } else {
+          scoreKeeper.add(
+            Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+          );
+          trueCount++;
         }
         quizBrain.nextQuestion();
         currentColorIndex = (currentColorIndex + 1) % questionTextColors.length;
+        _controller.restart();
       }
     });
   }
@@ -86,103 +102,156 @@ class _QuizPageState extends State<QuizPage> {
       trueCount = 0;
       falseCount = 0;
       currentColorIndex = 0;
+      _controller.pause();  // Pause the timer when resetting the quiz
+    });
+  }
+
+  void startQuiz() {
+    setState(() {
+      _controller.restart();  // Restart the timer when starting the quiz
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    return Stack(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Center(
-            child: Text(
-              'True: $trueCount | False: $falseCount',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.white,
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 70,
+                    height: 70,
+                    child: NeonCircularTimer(
+                      width: 70,
+                      duration: 5,
+                      controller: _controller,
+                      isTimerTextShown: true,
+                      neumorphicEffect: true,
+                      innerFillGradient: LinearGradient(colors: [
+                        Colors.greenAccent.shade200,
+                        Colors.blueAccent.shade400,
+                      ]),
+                      neonGradient: LinearGradient(colors: [
+                        Colors.greenAccent.shade200,
+                        Colors.blueAccent.shade400,
+                      ]),
+                      onComplete: () => checkAnswer(null),
+                      textFormat: TextFormat.S,
+                    ),
+                  ),
+                  Text(
+                    'True: $trueCount | False: $falseCount',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ),
-        Expanded(
-          flex: 6,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                quizBrain.getQuestionText() ?? '',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: questionTextColors[currentColorIndex],
+            Expanded(
+              flex: 6,
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Center(
+                  child: Text(
+                    quizBrain.getQuestionText() ?? '',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: questionTextColors[currentColorIndex],
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                foregroundColor: Colors.white,
-              ),
-              onPressed: () {
-                // The user picked true.
-                checkAnswer(true);
-              },
-              child: Text(
-                'True',
-                style: TextStyle(
-                  fontSize: 20.0,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                  ),
+                  onPressed: () {
+                    // The user picked true.
+                    _controller.pause();
+                    checkAnswer(true);
+                  },
+                  child: Text(
+                    'True',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
-              onPressed: () {
-                // The user picked false.
-                checkAnswer(false);
-              },
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  onPressed: () {
+                    // The user picked false.
+                    _controller.pause();
+                    checkAnswer(false);
+                  },
+                  child: Text(
+                    'False',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-        Row(
-          children: scoreKeeper,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
+            Row(
+              children: scoreKeeper,
             ),
-            onPressed: resetQuiz,
-            child: Text(
-              'Reset Quiz',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: resetQuiz,
+                child: Text(
+                  'Reset Quiz',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                ),
+                onPressed: startQuiz,
+                child: Text(
+                  'Start Quiz',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ],
     );
